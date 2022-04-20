486660821fbc4d915160a715661f1de514cff7c2
ekorina <alnitak.nik@gmail.com>
2022-04-20 14:45:36 +0200

    Turn counter.

--- godot/Room.gd
+++ godot/Room.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+var turn = 0
+
 func _ready():
 	Events.connect('mortal_about_to_move', self, '_on_mortal_about_to_move')
 	Events.connect('mortal_move_start', self, '_on_mortal_move_start')
+	Events.connect('mortal_turn_end', self, '_on_mortal_turn_end')
 	Events.connect('game_over', self, '_on_game_over')
 	
 	randomize()
+	self.add_turn()
 	
 	yield(get_tree().create_timer(1), "timeout")
 	$Hand.draw('X2')
@@ -26,7 +30,10 @@ func _on_mortal_move_start():
 	$Path.move_mortal()
 	yield($Path, 'mortal_moved')
 	
-	# Events.emit_signal("mortal_turn_end")
+	Events.emit_signal("mortal_turn_end")
+	
+func _on_mortal_turn_end():
+	self.add_turn()
 	Events.emit_signal("mortal_turn_start")
 	
 func angel_turn_start():
@@ -38,4 +45,8 @@ func angel_turn_end():
 	
 func _on_game_over():
 	print('GAME OVER')
+	
+func add_turn():
+	turn += 1
+	$TurnLabel.text = 'Turn ' + str(turn)
 	
